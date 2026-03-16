57179ae10db602ac8e1e578a3ae07661ba8925b7
優希 秋元 <250579@jc-21.jp>
2026-03-16 11:02:43 +0900

    ステージ拡大前

--- Stage.cpp
+++ Stage.cpp
@@ -6,7 +6,7 @@ namespace {
 }
 Stage::Stage()
 {
-	hImage= LoadGraph("data/Image/stage/床イメージ.2.png");
+	hImage= LoadGraph("data/Image/stage/TileImage.png");
 	CsvReader* csv = new CsvReader("data/Image/stage/stage1-1.csv");
 	int lines = csv->GetLines();//行数を取得
 	map.resize(lines);//mapの行数を設定
@@ -35,10 +35,10 @@ void Stage::Draw()
 	for (int y = 0; y < map.size(); y++) {
 		for (int x = 0; x < map[y].size(); x++) {
 			if (map[y][x] == 1) {
-				DrawRectGraph(IMAGE_SCALE* x - scrollX, y * IMAGE_SCALE, 0,IMAGE_SCALE, IMAGE_SCALE, IMAGE_SCALE, hImage, true);
+				DrawRectGraph(IMAGE_SCALE* x - scrollX, y * IMAGE_SCALE, 0,0, IMAGE_SCALE, IMAGE_SCALE, hImage, true);
 			}
 			else if (map[y][x] == 2) {
-				DrawRectGraph(IMAGE_SCALE*x - scrollX, y * IMAGE_SCALE,IMAGE_SCALE, IMAGE_SCALE,IMAGE_SCALE, IMAGE_SCALE, hImage, true);
+				DrawRectGraph(IMAGE_SCALE*x - scrollX, y * IMAGE_SCALE,0,0,IMAGE_SCALE, IMAGE_SCALE, hImage, true);
 			}
 		}
 	}
