81a9ac71c56539c2197288daa8bfd0012d676de4
shojiMakotoX <250039@jc-21.jp>
2026-03-16 10:17:46 +0900

    微調整

--- Character.h
+++ Character.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Object.h"
-class Character : Object
+class Character : public Object
 {
 public:
 	Character();
--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,4 @@
 #include "Player.h"
-#include "Object.h"
 
 Player::Player()
 {
