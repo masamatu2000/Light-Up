e25f66648990a21d8ed7e94b3b0b043892fa84c2
ha sugi <suginuma1@outlook.jp>
2026-03-17 10:24:12 +0900

    マップの名前をCSVから読み込み完了

--- Stage.h
+++ Stage.h
@@ -15,5 +15,7 @@ private:
 	void Update() override;
 	void Draw() override;
 private:
+	std::vector<std::string> mapName; //マップの名前保存用
+	std::string mapReference; //マップの参照保存用
 	std::vector<std::vector<int>> map;//CSV保存用
 };
