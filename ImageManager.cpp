97a14fdf012bd60519c6a59e1324ed645f2fad17
優希 秋元 <250579@jc-21.jp>
2026-03-17 13:52:18 +0900

    ImageManagerつくりますた

--- ImageManager.cpp
+++ ImageManager.cpp
@@ -1,9 +1,35 @@
 #include "ImageManager.h"
-
+#include"CsvReader.h"
 Image::Image()
 {
+	CsvReader* csv = new CsvReader("ImageName.csv");
+	int lines = csv->GetLines();//行数を取得
+	Images.resize(lines);//mapの行数を設定
+	for (int y = 0; y < lines; y++) {//1行ずつ読む
+		int colos = csv->GetColumns(y);//その行の桁数を取得
+		Images[y].resize(colos);//mapのその行の桁数を設定
+		for (int x = 0; x < colos; x++) {
+			Images[y][x] = csv->GetInt(y, x);//mapに値を設定
+		}
+	}
+	delete csv;
 }
 
 Image::~Image()
 {
+}
+
+int Image::ReturnImage(std::string Imname)
+{
+	int i = 0;
+	int target = stoi(Imname);
+	// Imagesは2次元配列なので、全ての値を探索する必要があります
+	for (size_t y = 0; y < Images.size(); ++y) {
+		for (size_t x = 0; x < Images[y].size(); ++x) {
+			if (target == Images[y][x]) {
+				return LoadGraph((const TCHAR*)Images[y][x]);
+			}
+		}
+	}
+	return 0;
 }
--- ImageManager.h
+++ ImageManager.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "GameObject.h"
-
+#include<vector>
+#include<string.h>
 class Image : GameObject
 {
 public:
 	Image();
 	~Image();
+	int ReturnImage(std::string Imname);
+private:
+	std::vector<std::vector<int>> Images;
 };
