cdb68344d66a695436ce5d8d6cfdc5167c201ff7
優希 秋元 <250579@jc-21.jp>
2026-03-23 09:16:35 +0900

    Enemy追加

--- EnemyMove.h
+++ EnemyMove.h
@@ -1,5 +1,7 @@
 #pragma once
-
+/// <summary>
+/// akimotomasaki
+/// </summary>
 enum ENEMY_NUMBER
 {
 	Enemy01,
@@ -9,3 +11,10 @@ enum ENEMY_NUMBER
 	Enemy05,
 	Enemy06,
 };
+namespace EnemyAttack {
+	void Enemy1Attack(const Vector2D& pos);
+	void Enemy2Attack(const Vector2D& pos);
+	void Enemy3Attack(const Vector2D& pos);
+	void Enemy4Attack(const Vector2D& pos);
+	void Enemy5Attack(const Vector2D& pos);
+}
