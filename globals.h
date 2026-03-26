a2de5fc05f2fe10bdf97c9ce07158d66e19f4826
冴空 大嶋 <sorauso900@gmail.com>
2026-03-26 11:45:15 +0900

    プレイヤーごとの攻撃

--- globals.h
+++ globals.h
@@ -6,7 +6,7 @@ const int WIN_HEIGHT = 1080;
 //最終的に何倍のサイズにするか
 //---画像を維持するために整数以外入れるな)---
 //縦に１０マスくらいだとZOOMは3（１１．２５マスくらい）
-const int SCREEN_ZOOM = 4;
+const int SCREEN_ZOOM = 3;
 const char IMAGE_SCALE = 32;
 
 extern float gDeltaTime; // フレーム間の時間差
